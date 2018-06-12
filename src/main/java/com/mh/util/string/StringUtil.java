8c5152a6be4d447fb11f0bf5cab76c5475bb54f9
menghan <menghan_sl@163.com>
2018-06-12 14:07:13 +0800

    字符工具类-添加云除空白和换行符

--- src/main/java/com/mh/util/string/StringUtil.java
+++ src/main/java/com/mh/util/string/StringUtil.java
@@ -1,5 +1,8 @@
 package com.mh.util.string;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Administrator on 2017/5/23.
  */
@@ -26,6 +29,22 @@ public class StringUtil {
         newChars[0] = newChar;
         str.getChars(1, strLen, newChars, 1);
         return String.valueOf(newChars);
+    }
+
+    /**
+     * 去除空格和换行符的字符串
+     * @param str
+     * @return
+     */
+    public static String getStringNoBlank(String str) {
+        if (str != null && !"".equals(str)) {
+            Pattern p = Pattern.compile("\\s|\t|\r|\n");
+            Matcher m = p.matcher(str);
+            String strNoBlank = m.replaceAll("");
+            return strNoBlank;
+        } else {
+            return str;
+        }
     }
 
     public static void main(String[] args) {
